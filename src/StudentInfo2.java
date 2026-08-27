4943af02d2713410014280777bb15f48dfe5a338
josh <joshuahunter4455@gmail.com>
2026-08-27 10:18:23 -0400

    This is for AUG 27.

---
+++ src/StudentInfo2.java
@@ -0,0 +1,19 @@
+
+public class StudentInfo2 {
+
+    //Every java project will have only a and single main method
+    //Method = grouping of relevant code+
+    // Return_Type (Void) Method_name/(main) (Parameter_Type Parameter_Name){
+    // Relevant Codes
+    // }
+    // Declare Attributes
+    String FN;
+    String LN;
+    int ID;
+    double CGP;
+    String address;
+    public static void main(String[] args){
+
+        System.out.println("Hello Java!");
+
+    }   }
